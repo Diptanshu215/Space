318a70c265bf2140377d0205091ea9f72c194bb6
Diptanshu215 <145245833+Diptanshu215@users.noreply.github.com>
2024-07-16 00:17:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-document.getElementById('enter-button').addEventListener('click', function() {
-    document.getElementById('great-filter').scrollIntoView({ behavior: 'smooth' });
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('dot-container');
+    const numberOfDots = 100; // Adjust the number of dots as needed
+
+    for (let i = 0; i < numberOfDots; i++) {
+        const dot = document.createElement('div');
+        dot.classList.add('dot');
+        
+        const posX = Math.random() * window.innerWidth;
+        const posY = Math.random() * window.innerHeight;
+        
+        dot.style.left = `${posX}px`;
+        dot.style.top = `${posY}px`;
+
+        container.appendChild(dot);
+    }
+
+    document.getElementById('enter-button').addEventListener('click', function() {
+        document.getElementById('great-filter').scrollIntoView({ behavior: 'smooth' });
+    });
 });
